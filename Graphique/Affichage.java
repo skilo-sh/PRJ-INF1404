0330e73f8e4c2d32114f3cb7309b9f9156c52aaa
dysonv12 <jeuxfarluck@gmail.com>
2024-03-20 10:39:39 +0100

    fenetre

---
+++ Graphique/Affichage.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Affichage extends JFrame {
+
+   public Affichage() {
+      super("Ma Superbe App");
+
+      WindowListener l = new WindowAdapter() {
+         public void windowClosing(WindowEvent e){
+            System.exit(0);
+         }
+      };
+
+      addWindowListener(l);
+      setSize(200,100);
+      setVisible(true);
+   }
+}
