3ad3207acdca168fb3cf04068a8b1969d47267d5
skilo-sh <arbre488@gmail.com>
2024-03-24 20:24:14 +0100

    Adding Noeud.java

---
+++ Logique/Noeud.java
@@ -0,0 +1,23 @@
+// Je fais partit du package Logique
+package Logique;
+
+// J'ai besoin de ces classes pour travailler
+import Logique.Utils.TypeObstacle;
+import Logique.Utils.Vec2;
+
+/*
+	- Obtenir les enfants (directs, pas résoudre l'arbre complet)
+	- Lui associer une position/orientation
+	- 
+*/
+
+public class Noeud {
+	// Attributs
+
+	// Constructeur
+	public Noeud()
+	{
+	}
+
+	// Méthodes
+}
