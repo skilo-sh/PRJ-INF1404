2a011156fafd69844e0a5df4e676c0caf561cc11
skilo-sh <arbre488@gmail.com>
2024-03-24 11:08:09 +0100

    Adding Situation.java

---
+++ Logique/Situation.java
@@ -0,0 +1,18 @@
+// Je fais partit du package Logique
+package Logique;
+
+// J'ai besoin de ces classes pour travailler
+// import Logique.Utils.TypeObstacle;
+import Logique.Utils.Vec2;
+// import java.util.Stack;
+
+public class Situation {
+	// Attributs
+	Vec2 laserPosition;
+	
+
+	// Constructeur
+
+
+	// 
+}
